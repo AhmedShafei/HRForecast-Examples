2ef2e6a9f970836f61c9794a6b2162b209623fb3
Ahmed Shafei <Ahmed.Shafei@tum.de>
2014-11-12 12:38:31 +0100

    Initial Project commit

---
+++ GraphDemo/src/de/hrforecast/demo/main/MainActivity.java
@@ -0,0 +1,65 @@
+package de.hrforecast.demo.main;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import de.hrforecast.demo.william.ChartActivity;
+
+/**
+ * Launcher {@link ListActivity} responsible for launching the libraries intents
+ */
+public class MainActivity extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		String[] libraries = { "HelloChart", "HZ Graph", "AFreeChart",
+				"MP Android", "Eaze Graph", "Holograph", "William Charts",
+				"AChart Engine", "Android Plot" };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, libraries);
+		setListAdapter(adapter);
+	}
+
+	/**
+	 * Insert your activity intent here at your corresponding case number.
+	 */
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		switch (position) {
+		case 0:
+
+			break;
+		case 1:
+
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+		case 6:
+			this.startActivity(new Intent(this, ChartActivity.class));
+			break;
+		case 7:
+
+			break;
+		case 8:
+
+			break;
+		case 9:
+
+			break;
+		}
+	}
+}
---
+++ GraphDemo/src/de/hrforecast/demo/william/ChartListFragment.java
@@ -0,0 +1,74 @@
+package de.hrforecast.demo.william;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * {@link ListFragment} responsible for populating list of the supported chart
+ * types by the implemented library. It notifies the {@link ChartActivity}
+ * whenever new chart type is selected.
+ */
+public class ChartListFragment extends ListFragment {
+
+	private Callbacks listCallback;
+	private View tempView;
+
+	public interface Callbacks {
+		public void onChartSelected(int position);
+	}
+
+	public ChartListFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		/**
+		 * Edit this with the chart types you choose for your library
+		 * implementation
+		 */
+		String[] charts = { "Vertical Bar Chart", "Pie Chart",
+				"Horizontal Bar Chart", "Radar Chart", "Scatter Chart",
+				"etc..", "etc..", "etc..", "etc.." };
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				this.getActivity(), android.R.layout.simple_list_item_1, charts);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		this.getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		if (!(activity instanceof Callbacks))
+			throw new IllegalStateException(
+					"Activity must implement fragment's callbacks.");
+		listCallback = (Callbacks) activity;
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+	}
+
+	@Override
+	public void onListItemClick(ListView listView, View view, int position,
+			long id) {
+		super.onListItemClick(listView, view, position, id);
+		if (tempView != null)
+			tempView.setBackgroundResource(android.R.color.white);
+		view.setBackgroundResource(android.R.color.darker_gray);
+		tempView = view;
+		listCallback.onChartSelected(position);
+	}
+}
