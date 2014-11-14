6ff7efa29bb16fc57ab21a9cc0e9ae4b2c375641
Antoniq <a.atanasova30@gmail.com>
2014-11-14 15:46:35 +0100

    MPAndroid initial structure and new branch created for it

---
+++ GraphDemo/src/de/hrforecast/demo/mpandroid/ChartActivity.java
@@ -0,0 +1,37 @@
+package de.hrforecast.demo.mpandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import de.hrforecast.demo.R;
+
+/**
+ * {@link FragmentActivity} responsible for populating both the
+ * {@link ChartListFragment} and {@link ChartDetailFragment}. It receives
+ * callback from {@link ChartListFragment} whenever new chart type is selected,
+ * and notifies the {@link ChartDetailFragment}
+ */
+public class ChartActivity extends FragmentActivity implements
+		ChartListFragment.Callbacks {
+
+	private ChartDetailFragment chartDetailFragment;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chart_twopane);
+
+		ChartListFragment chartListFragment = new ChartListFragment();
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.charts_listContainer, chartListFragment).commit();
+
+		chartDetailFragment = new ChartDetailFragment();
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.charts_detailContainer, chartDetailFragment)
+				.commit();
+	}
+
+	@Override
+	public void onChartSelected(int position) {
+		chartDetailFragment.updateChartDetails(position);
+	}
+}
---
+++ GraphDemo/src/de/hrforecast/demo/mpandroid/ChartDetailFragment.java
@@ -0,0 +1,57 @@
+package de.hrforecast.demo.mpandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import de.hrforecast.demo.R;
+
+/**
+ * {@link Fragment} responsible for showing the details of the selected chart
+ * type. It receives a call whenever new chart type is selected
+ */
+public class ChartDetailFragment extends Fragment {
+
+	private int selectedChart;
+
+	public ChartDetailFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		/**
+		 * Edit this to inflate the corresponding layout of your
+		 * chartDetailFragment class
+		 */
+		View rootView = inflater.inflate(
+				R.layout.william_fragment_chart_detail, container, false);
+
+		return rootView;
+	}
+
+	/**
+	 * This method is called when a new chart type is selected
+	 * 
+	 * @param position
+	 *            It's the position of the selected chart type in the
+	 *            corresponding {@link ChartListFragment}
+	 */
+	public void updateChartDetails(int position) {
+		this.selectedChart = position;
+
+		/**
+		 * Dummy TextView, to be changed to the actual implementation of the
+		 * library charts
+		 */
+		((TextView) this.getView().findViewById(R.id.chart_detailText))
+				.setText("Chart in position " + selectedChart + " is selected");
+	}
+}
